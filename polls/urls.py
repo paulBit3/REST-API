8c12c239c8508b913990354b71026102126e4e1e
paulbit3 <paultechnologie@gmail.com>
2019-01-27 12:17:31 -0600

    swagger commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 from .apiviews import PollViewSet, ChoiceList, CreateVote, UserCreate, LoginView
 #from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 #from .views import polls_list, polls_detail
 
 
+schema_view = get_swagger_view(title='Polls API')
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
@@ -18,6 +20,7 @@ path('login/', LoginView.as_view(), name='login'),
 path('users/', UserCreate.as_view(), name='user_create'),
 path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
 path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote'),
+path('swagger-docs/', schema_view),
 # path('polls/', PollList.as_view(), name='polls_list'),
 # path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
 ]
